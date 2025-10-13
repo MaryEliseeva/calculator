b0b5d97e17e6f7399eeaf74697fc934e8af4d6cc
MaryEliseeva <marymaryeliseeva@gmail.com>
2025-10-13 20:48:17 +0300

    init proj

---
+++ calculator/calculator.cpp
@@ -0,0 +1,12 @@
+﻿// calculator.cpp : Defines the entry point for the application.
+//
+
+#include "calculator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
